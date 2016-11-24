bdc1f6738bb34f6ea9a5e4b5b6cd0295726434c2
grammophone <emmanuel.liossis@gmail.com>
2016-11-24 22:49:30 +0200

    Initial contract definition.

---
+++ IRenderProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.TemplateRendering
+{
+	/// <summary>
+	/// Contract for template rendering implementations,
+	/// which must be thread-safe.
+	/// </summary>
+	public interface IRenderProvider : IDisposable
+	{
+		/// <summary>
+		/// Render a template using a strong-type <paramref name="model"/>.
+		/// </summary>
+		/// <typeparam name="M">The type of the model.</typeparam>
+		/// <param name="templateKey">The key of the template.</param>
+		/// <param name="textWriter">The writer used for output.</param>
+		/// <param name="model">The model.</param>
+		/// <param name="dynamicProperties">Optional dynamic properties.</param>
+		void Render<M>(
+			string templateKey,
+			TextWriter textWriter,
+			M model,
+			IDictionary<string, object> dynamicProperties = null);
+
+		/// <summary>
+		/// Render a template.
+		/// </summary>
+		/// <param name="templateKey">The key of the template.</param>
+		/// <param name="textWriter">The writer used for output.</param>
+		/// <param name="dynamicProperties">The dynamic properties.</param>
+		void Render(
+			string templateKey,
+			TextWriter textWriter,
+			IDictionary<string, object> dynamicProperties);
+	}
+}
